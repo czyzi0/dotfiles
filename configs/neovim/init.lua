d7383b24a0aef9a028f07a714466c519742dcf21
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2023-12-21 21:01:58 +0100

    Add plugin manager and some basic plugins

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -4,7 +4,69 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- TODO: Plugin manager
+-- Install `lazy.nvim` (https://github.com/folke/lazy.nvim) - `:help lazy.nvim.txt`
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Configure plugins
+require('lazy').setup({
+  -- Detect tabstops and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- Color scheme - `:help sonokai.txt`
+  {
+    'sainnhe/sonokai',
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_style = 'default'
+      vim.g.sonokai_better_performance = 1
+      vim.g.sonokai_transparent_background = 1
+      vim.cmd.colorscheme 'sonokai'
+    end,
+  },
+
+  -- Use lualine as statusline - `:help lualine.txt`
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'sonokai',
+        component_separators = '│',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = {
+          function()
+            if vim.o.expandtab then
+              return 'spaces: ' .. tostring(vim.o.shiftwidth)
+            else
+              return 'tabs: ' .. tostring(vim.o.tabstop)
+            end
+          end,
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    },
+  },
+}, {})
 
 -- Disable wrapping
 vim.wo.wrap = false
@@ -24,6 +86,12 @@ vim.opt.tabstop = 4       -- Set width of tab
 vim.opt.shiftwidth = 4    -- Set indents width
 vim.bo.softtabstop = 4    -- Set number of columns for tab
 vim.opt.expandtab = true  -- Expand tabs to spaces
+
+--
+vim.o.fillchars = 'vert:│'
+
+-- Disable netrw banner
+vim.g.netrw_banner = 0
 
 -- Set highlight on search
 vim.o.hlsearch = false
