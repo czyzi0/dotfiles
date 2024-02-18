6339a8dd96259862b334efce5473a8ac48d704bb
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2024-02-18 09:14:43 +0100

    Add fun make it rain plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,18 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 
 -- Install `lazy.nvim` (https://github.com/folke/lazy.nvim) - `:help lazy.nvim.txt`
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   }
 end
@@ -20,109 +20,117 @@ vim.opt.rtp:prepend(lazypath)
 
 
 -- Configure plugins
-require('lazy').setup({
+require("lazy").setup({
   -- Detect tabstops and shiftwidth automatically
-  'tpope/vim-sleuth',
+  "tpope/vim-sleuth",
 
   -- Delete buffers without changing window layout
-  'moll/vim-bbye',
+  "moll/vim-bbye",
 
   -- Easy commenting - https://github.com/numToStr/Comment.nvim
   {
-    'numToStr/Comment.nvim',
+    "numToStr/Comment.nvim",
     opts = { mappings = { extra = false } },
   },
 
   -- Markdown preview
   {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && npm install',
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
     config = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_filetypes = { "markdown" }
       vim.g.mkdp_open_to_the_world = 1
       vim.g.mkdp_echo_preview_url = 1
-      vim.g.mkdp_theme = 'dark'
+      vim.g.mkdp_theme = "dark"
     end,
-    ft = { 'markdown' },
+    ft = { "markdown" },
   },
 
   -- Color scheme - `:help sonokai.txt`
   {
-    'sainnhe/sonokai',
+    "sainnhe/sonokai",
     priority = 1000,
     config = function()
-      vim.g.sonokai_style = 'default'
+      vim.g.sonokai_style = "default"
       vim.g.sonokai_better_performance = 1
       vim.g.sonokai_transparent_background = 1
-      vim.cmd.colorscheme 'sonokai'
+      vim.cmd.colorscheme "sonokai"
     end,
   },
 
   -- Use lualine as statusline - `:help lualine.txt`
   {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'sonokai',
-        component_separators = '│',
-        section_separators = '',
+        theme = "sonokai",
+        component_separators = "│",
+        section_separators = "",
       },
       sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        lualine_c = { 'filename' },
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { "filename" },
         lualine_x = {
           function()
             if vim.o.expandtab then
-              return 'spaces: ' .. tostring(vim.o.shiftwidth)
+              return "spaces: " .. tostring(vim.o.shiftwidth)
             else
-              return 'tabs: ' .. tostring(vim.o.tabstop)
+              return "tabs: " .. tostring(vim.o.tabstop)
             end
           end,
-          'encoding',
-          'fileformat',
-          'filetype',
+          "encoding",
+          "fileformat",
+          "filetype",
         },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
     },
   },
 
   -- Better highlighting and navigation for code
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    build = ':TSUpdate',
+    build = ":TSUpdate",
   },
 
   -- LSP stuff
   {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
       -- Package managers for LSPs
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
     },
   },
 
   -- Autocompletion things
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
       -- Snippet engine and completion source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
 
       -- Autocompletion from LSP
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
     },
+  },
+
+  -- Useless plugin for making rain
+  {
+    "eandrju/cellular-automaton.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>mir", ":CellularAutomaton make_it_rain<CR>")
+    end
   },
 
 }, {})
@@ -133,7 +141,7 @@ vim.g.netrw_banner = 0
 -- Use basic style listing in netrw
 vim.g.netrw_liststyle = 0
 -- Show line numbers in netrw
-vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
+vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 
 -- Make line numbers default
 vim.wo.number = true
@@ -141,12 +149,12 @@ vim.wo.relativenumber = true
 -- Disable wrapping
 vim.wo.wrap = false
 -- Show whitespaces
-vim.opt.listchars:append({ eol = '¬', space = '·', tab = '→ ' })
+vim.opt.listchars:append({ eol = "¬", space = "·", tab = "→ " })
 vim.opt.list = true
 -- Set encoding
-vim.opt.encoding = 'utf-8'
+vim.opt.encoding = "utf-8"
 -- Show rulers
-vim.opt.colorcolumn = '79,100'
+vim.opt.colorcolumn = "79,100"
 -- Set scrolloffs
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
@@ -164,7 +172,7 @@ vim.opt.splitright = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 -- Recursive searching
-vim.opt.path:append '**'
+vim.opt.path:append "**"
 
 -- Show wildcard menu
 vim.opt.wildmenu = true
@@ -173,12 +181,12 @@ vim.opt.wildmenu = true
 vim.o.hlsearch = false
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+--  See `:help "clipboard"`
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -187,7 +195,7 @@ vim.o.breakindent = true
 vim.o.undofile = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -200,87 +208,87 @@ vim.o.complete = false
 vim.o.termguicolors = true
 
 -- Use groovy syntax for highlighting Jenkinsfile
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-  pattern = { 'Jenkinsfile' },
-  callback = function() vim.o.filetype = 'groovy' end,
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "Jenkinsfile" },
+  callback = function() vim.o.filetype = "groovy" end,
 })
 
 
 -- Easy split creation
-vim.keymap.set('n', '<leader>v', ':Vex!<CR>', { desc = 'Split right' })
-vim.keymap.set('n', '<leader>h', ':Hex<CR>', { desc = 'Split below' })
+vim.keymap.set("n", "<leader>v", ":Vex!<CR>", { desc = "Split right" })
+vim.keymap.set("n", "<leader>h", ":Hex<CR>", { desc = "Split below" })
 -- Easy split navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to split left' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to split below' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to split above' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to split right' })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to split left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to split below" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to split above" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to split right" })
 -- Easy vertical split resizing
-vim.keymap.set('n', '<C-Left>', ':vertical resize +3<CR>', { desc = 'Resize vertical split' })
-vim.keymap.set('n', '<C-Right>', ':vertical resize -3<CR>', { desc = 'Resize vertical split' })
+vim.keymap.set("n", "<C-Left>", ":vertical resize +3<CR>", { desc = "Resize vertical split" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize -3<CR>", { desc = "Resize vertical split" })
 -- Easy horizontal split resizing
-vim.keymap.set('n', '<C-Up>', ':horizontal resize +1<CR>', { desc = 'Resize horizontal split' })
-vim.keymap.set('n', '<C-Down>', ':horizontal resize -1<CR>', { desc = 'Resize horizontal split' })
+vim.keymap.set("n", "<C-Up>", ":horizontal resize +1<CR>", { desc = "Resize horizontal split" })
+vim.keymap.set("n", "<C-Down>", ":horizontal resize -1<CR>", { desc = "Resize horizontal split" })
 
 -- Easy buffer navigation
-vim.keymap.set('n', '<leader>,', ':bprevious<CR>')
-vim.keymap.set('n', '<leader>.', ':bnext<CR>')
+vim.keymap.set("n", "<leader>,", ":bprevious<CR>")
+vim.keymap.set("n", "<leader>.", ":bnext<CR>")
 
 -- Adding empty line below or above in normal mode
-vim.keymap.set('n', '<leader>o', ':call append(line("."), "")<CR>', { desc = 'Empty line below in normal mode' })
-vim.keymap.set('n', '<leader>O', ':call append(line(".")-1, "")<CR>', { desc = 'Empty line above in normal mode' })
+vim.keymap.set("n", "<leader>o", ":call append(line(\".\"), \"\")<CR>", { desc = "Empty line below in normal mode" })
+vim.keymap.set("n", "<leader>O", ":call append(line(\".\")-1, \"\")<CR>", { desc = "Empty line above in normal mode" })
 
 -- Easy search for files
-vim.keymap.set('n', '<leader>f', ':find ', { desc = 'Search for file' })
+vim.keymap.set("n", "<leader>f", ":find ", { desc = "Search for file" })
 
 -- Center cursor after scrolling half the page
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Diagnostic messages
-vim.keymap.set('n', '<leader>d[', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', '<leader>d]', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Show diagnostics list' })
-vim.keymap.set('n', '<A-k>', vim.diagnostic.open_float, { desc = 'Show diagnostic info' })
+vim.keymap.set("n", "<leader>d[", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "<leader>d]", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Show diagnostics list" })
+vim.keymap.set("n", "<A-k>", vim.diagnostic.open_float, { desc = "Show diagnostic info" })
 
 -- Move selected lines up and down
-vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
-vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
+vim.keymap.set("v", "J", ":m \">+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m \"<-2<CR>gv=gv")
 
 -- Easy global replace - TODO: Check if this is helpful
-vim.keymap.set('v', '<leader>r', '"hy:%s/<C-r>h//g<left><left>')
+vim.keymap.set("v", "<leader>r", "\"hy:%s/<C-r>h//g<left><left>")
 
 -- Disable space in normal and visual modes
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<NOP>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<NOP>", { silent = true })
 
 -- For habit breaking
-vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', '<NOP>')
-vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', '<NOP>')
-vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<NOP>')
-vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<NOP>')
+vim.keymap.set({ "n", "i", "v" }, "<Up>", "<NOP>")
+vim.keymap.set({ "n", "i", "v" }, "<Down>", "<NOP>")
+vim.keymap.set({ "n", "i", "v" }, "<Left>", "<NOP>")
+vim.keymap.set({ "n", "i", "v" }, "<Right>", "<NOP>")
 
 
 -- Configure treesitter - `:help nvim-treesitter`
--- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+-- Defer Treesitter setup after first render to improve startup time of "nvim {filename}"
 -- `:TSInstallInfo` for more info, `:TSInstall ...` to install new language
 vim.defer_fn(function()
-  require('nvim-treesitter.configs').setup {
+  require("nvim-treesitter.configs").setup {
     -- Select supported languages
     ensure_installed = {
-      'bash',
-      'c',
-      'cpp',
-      'css',
-      'dockerfile',
-      'groovy',
-      'html',
-      'javascript',
-      'json',
-      'latex',
-      'lua',
-      'markdown',
-      'python',
-      'vimdoc',
-      'vim',
+      "bash",
+      "c",
+      "cpp",
+      "css",
+      "dockerfile",
+      "groovy",
+      "html",
+      "javascript",
+      "json",
+      "latex",
+      "lua",
+      "markdown",
+      "python",
+      "vimdoc",
+      "vim",
     },
     -- Do not autoinstall languages that are not installed
     auto_install = false,
@@ -292,33 +300,33 @@ end, 0)
 
 
 -- LSP configuration
-require('mason').setup()
+require("mason").setup()
 
 local on_attach = function(_, bufnr)
   -- Keymaps
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
-  vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
-  vim.keymap.set('n', '<leader>gl', vim.lsp.buf.references, { desc = 'Show references list' })
-  vim.keymap.set('n', '<leader>gr', vim.lsp.buf.rename, { desc = 'Rename' })
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+  vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+  vim.keymap.set("n", "<leader>gl", vim.lsp.buf.references, { desc = "Show references list" })
+  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { desc = "Rename" })
   -- Documentation
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show documentation' })
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { desc = 'Show documentation' })
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentation" })
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Show documentation" })
 
   -- Command `:Format` local to the buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
     vim.lsp.buf.format()
-  end, { desc = 'Format current buffer' })
+  end, { desc = "Format current buffer" })
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local servers = {
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
-      diagnostics = { globals = { 'vim' } },
+      diagnostics = { globals = { "vim" } },
     },
   },
   pyright = {},
@@ -326,7 +334,7 @@ local servers = {
 
 local handlers = {
   function(server_name)
-    require('lspconfig')[server_name].setup({
+    require("lspconfig")[server_name].setup({
       capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
@@ -335,30 +343,30 @@ local handlers = {
   end,
 }
 
-require('mason-lspconfig').setup({
+require("mason-lspconfig").setup({
   ensure_installed = vim.tbl_keys(servers),
   handlers = handlers,
 })
 
 
 -- Autocompletion configuration - `:help cmp`
-local cmp = require('cmp')
-local luasnip = require('luasnip')
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 luasnip.config.setup({})
 cmp.setup({
   snippet = {
     expand = function(args) luasnip.lsp_expand(args.body) end,
   },
   completion = {
-    completeopt = 'menu,menuone,noinsert',
+    completeopt = "menu,menuone,noinsert",
   },
   mapping = {
-    ['<C-Space>'] = cmp.mapping.complete({}),
-    ['<CR>'] = cmp.mapping.confirm({
+    ["<C-Space>"] = cmp.mapping.complete({}),
+    ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_locally_jumpable() then
@@ -366,8 +374,8 @@ cmp.setup({
       else
         fallback()
       end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.locally_jumpable(-1) then
@@ -375,12 +383,12 @@ cmp.setup({
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
   },
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'luasnip' },
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+    { name = "luasnip" },
   },
 })
